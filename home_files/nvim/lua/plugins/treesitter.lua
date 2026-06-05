0f68e94d060671a1d12b62ffca0541512a7724e1
Liam Smith <liamjsmith02@protonmail.com>
2026-06-05 15:48:53 +0100

    Latest

--- home_files/nvim/lua/plugins/treesitter.lua
+++ home_files/nvim/lua/plugins/treesitter.lua
@@ -1,12 +1,13 @@
 return {
   'nvim-treesitter/nvim-treesitter',
   branch = 'master',
+  pin = true,
   build = ":TSUpdate",
   config = function()
     local config = require('nvim-treesitter.configs')
     config.setup({
       auto_install = true,
-      ensure_installed = {"lua", "javascript", "python", "c", "bash", "typescript", "ruby"},
+      ensure_installed = {"lua", "javascript", "python", "c", "bash", "typescript", "ruby"}, 
       highlight = {enable = true},
       indent = {enable = true},
     })
